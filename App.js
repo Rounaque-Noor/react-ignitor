2833933b415d56a8cefc703a7ec3de23ffe87149
Rounaque <rounaque7893@gmail.com>
2024-07-31 01:43:20 +0530

    Parcel is a beast

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
+
 
 const parent = React.createElement(
   "div",
   { id: "parent" },[
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello"),
+    React.createElement("h1", { id: "heading" }, "Hello World from Parcel React"),
     React.createElement("h2", { id: "heading2" }, "Hello"),
   ]),
   React.createElement("div", { id: "child2" }, [
