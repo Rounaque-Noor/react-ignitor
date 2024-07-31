3271d546f3cc3000d72d93650698b5d4876b4cd3
Rounaque <rounaque7893@gmail.com>
2024-07-31 20:17:57 +0530

    React element vs component and component coposition

--- App.js
+++ App.js
@@ -6,21 +6,35 @@ import ReactDOM from "react-dom/client";
 // root.render(heading);
 
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },[
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from Parcel React"),
-    React.createElement("h2", { id: "heading2" }, "Hello"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Hello"),
-    React.createElement("h2", { id: "heading2" }, "Hello"),
-  ])
-]
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },[
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading" }, "Hello World from Parcel React"),
+//     React.createElement("h2", { id: "heading2" }, "Hello"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", { id: "heading" }, "Hello"),
+//     React.createElement("h2", { id: "heading2" }, "Hello"),
+//   ])
+// ]
+// );
+
+//React Element
+jsxHeading = <h1 id="head" className="heading">Hello React JSX</h1>;
+
+//React Component
+const Heading2 = () => <h3>Hello Nested "Component Composition" .</h3>;
+
+//Component Composition (nested)
+const HeadingComponent = () => (
+  <div>
+    <Heading2 />
+    <h1>Hello React from React Component</h1>
+  </div>
 );
 
-console.log(parent);
+// console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
