c1898a4606a6a56e56cd38059dd99eee53dfed74
Rounaque <rounaque7893@gmail.com>
2024-08-05 00:47:22 +0530

    Search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,11 @@
 import { useEffect, useState } from "react";
 import Restaurant from "./Restaurant"
 
-const resList =
-{
-    resName: "KFC",
-    cuisine: "Burger, Fast Food",
-    quantity: "400 for two",
-    deliveryTime: 38
-}
-
 const Body = () => {
     const [listOfRes, setListOfRes] = useState([]);
+    const [filteredList, setfilteredList] = useState([]);
+    const [searchText, setsearchText] = useState("");
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -19,20 +14,29 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         setListOfRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
     return (
         <div>
-            <div>
+            <div className="upper-body">
                 <button className="filter-btn" onClick={
                     () => {
                         const filteredList = listOfRes.filter((res) => res.info.sla.deliveryTime < 30);
-                        setListOfRes(filteredList);
+                        setfilteredList(filteredList);
                     }
                 }>Fastest delivery Restaurant in your Area</button>
+                <div>
+                    <input type="text" value={searchText} onChange={ (e) => setsearchText(e.target.value)}/>
+                    <button onClick={() => {
+                        const searchedList = listOfRes.filter( (res) => res.info.name.toLowerCase().includes(searchText.toLocaleLowerCase()));
+                        setfilteredList(searchedList);
+                    }} className="filter-btn">Search</button>
+
+                </div>
             </div>
             <div className="res-container">
-                {listOfRes.map((restaurant) => (
+                {filteredList.map((restaurant) => (
                     <Restaurant key={restaurant.info.id} resData={{ restaurant }} />
                 ))}
             </div>
